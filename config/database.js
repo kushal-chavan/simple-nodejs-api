440de9c21d63f3e5843ae97d877fab9b9db6e75f
gitprojectsync <64169634+gitprojectsync@users.noreply.github.com>
2020-05-14 02:50:59 +0530

    config changes

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb+srv://manblog:manblog@tigerweb-opbe5.mongodb.net/test?retryWrites=true&w=majority',
+    database: 'mongodb+srv://manblog:manblog@tigerweb-opbe5.mongodb.net/rakeshchouhan?retryWrites=true&w=majority',
     secret: 'your_key'
 }
