0232e3f0fb26313a9064b193d1d832e5e3e3d7f1
kushal chavan <kushal.chavan120@gmail.com>
2020-08-16 01:47:11 +0530

    fixes

--- models/title.js
+++ models/title.js
@@ -7,6 +7,6 @@ const TitleSchema = mongoose.Schema({
 const Title = module.exports = mongoose.model('title', TitleSchema);
 
 // GET ALL POST
-Title.exports.getPost = (callback) => {
+module.exports.getPost = (callback) => {
 	Title.find(callback);
 }
